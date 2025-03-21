c4f5f1bd9f73d75d675bc0f7c4c934dc8f5d0c96
oliveiraspedro <pedro.santosdeoliveira13@gmail.com>
2025-03-20 23:13:15 -0300

    Initial commit

---
+++ src/main/java/dio/decola_tech_2025/DecolaTech2025Application.java
@@ -0,0 +1,13 @@
+package dio.decola_tech_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolaTech2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolaTech2025Application.class, args);
+	}
+
+}
